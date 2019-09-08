343c445a2e453cef0e277bc363d37436198fafab
luanvan-2019 <motivation1097@gmail.com>
2019-09-08 12:08:42 +0700

    slide

--- app/src/main/java/com/example/coosincustomer/HomeActivity.java
+++ app/src/main/java/com/example/coosincustomer/HomeActivity.java
@@ -4,16 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.coosincustomer.Adapter.ViewPagerAdapter;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class HomeActivity extends AppCompatActivity {
 
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        ViewPager viewPager = findViewById(R.id.viewPager);
-        ImageAdapter adapter = new ImageAdapter(this);
-        viewPager.setAdapter(adapter);
+        viewPager = findViewById(R.id.viewpager_image);
+        viewPager.setAdapter(viewPagerAdapter);
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new MyTimerTask(),2000,4000);
+    }
+
+    public class MyTimerTask extends TimerTask{
+
+        @Override
+        public void run() {
+            HomeActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                        if (viewPager.getCurrentItem() == viewPagerAdapter.getCount() - 1) {
+                            viewPager.setCurrentItem(0);
+                        } else {
+                            viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+                        }
+                }
+            });
+        }
     }
 }
