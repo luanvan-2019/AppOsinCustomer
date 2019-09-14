1870497dfa69143bccec7957725442f05debc3da
luanvan-2019 <motivation1097@gmail.com>
2019-09-14 21:32:01 +0700

    ConfirmLayout

---
+++ app/src/main/java/com/example/coosincustomer/ThanhtoanActivity.java
@@ -0,0 +1,14 @@
+package com.example.coosincustomer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThanhtoanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thanhtoan);
+    }
+}
